92f6d645331fe6d883987aa9f31e37eef67675b5
david <christian.murillo@fletx.co>
2022-11-20 22:06:03 -0500

    guard routes

--- src/app/main/services/pokemon.service.ts
+++ src/app/main/services/pokemon.service.ts
@@ -9,20 +9,21 @@ export class PokemonService {
 
   baseUrl: string = environment.apiUrl;
   public user = JSON.parse(localStorage.getItem('currentUser'))
+  public headers_object = new HttpHeaders().set("Authorization", "Bearer " + this.user.token);
 
   constructor(private http: HttpClient) { }
 
   //Obtiene pokemon
   getPokemons(index){
     
-    return this.http.get<any>(`${this.baseUrl}/api/users/pokemon/${index}`);
+    return this.http.get<any>(`${this.baseUrl}/api/users/pokemon/${index}`,{headers:this.headers_object});
   }
 
   pokemonDetails(idPokemon){
     
     console.log('***',this.user);
-    var headers_object = new HttpHeaders().set("Authorization", "Bearer " + this.user.token);
-    return this.http.get<any>(`${this.baseUrl}/api/users/pokemon-details/${idPokemon}`,{headers:headers_object});
+
+    return this.http.get<any>(`${this.baseUrl}/api/users/pokemon-details/${idPokemon}`,{headers:this.headers_object});
 
   }
 
