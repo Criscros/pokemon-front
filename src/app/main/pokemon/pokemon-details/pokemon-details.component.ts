56dd255bfc03626fb5906fb3633b4af2f629fdd8
david <christian.murillo@fletx.co>
2022-11-20 21:59:02 -0500

    fin

--- src/app/main/pokemon/pokemon-details/pokemon-details.component.ts
+++ src/app/main/pokemon/pokemon-details/pokemon-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { type } from 'os';
 import { PokemonService } from '../../services/pokemon.service'
 
 @Component({
@@ -12,6 +13,9 @@ export class PokemonDetailsComponent implements OnInit {
   public contentHeader: object
   public url = this.router.url;
   public idPokemon;
+  public todo;
+  public abilities = []
+  public characteristics
 
  /**
    * Constructor
@@ -28,37 +32,53 @@ export class PokemonDetailsComponent implements OnInit {
     // HEADER
     var idPokemon = this.url.substr(this.url.lastIndexOf('/') + 1);
 
+    this.todo ={
+      name : '',
+      type: ''
+    }
+
     this.contentHeader = {
-      headerTitle: 'Home',
+      headerTitle: 'Pokemon',
       actionButton: true,
       breadcrumb: {
         type: '',
         links: [
           {
-            name: 'Home',
+            name: 'pokemons',
             isLink: true,
             link: '/'
           },
-          {
-            name: 'Sample',
-            isLink: false
-          }
+  
         ]
       }
     }
 
     // SERVICE 
-
+    this.getPokemonInfo(idPokemon)    
+  }
+  getPokemonInfo(idPokemon){
+  
     this.pokemonService.pokemonDetails(idPokemon).subscribe(
       res => {
 
         console.log('RESPONSE....',res);
+        const todo = {
+          name: res.data.info.name,
+          image: res.data.info.sprites.front_default,
+          type: res.data.info.types[0].type
+        }
+
+        this.todo = todo 
+        this.abilities = res.data.info.abilities
+        this.characteristics = res.data.characteristic.descriptions[5].description
+
+
+        console.log('todo',todo);
       },
       err => {
         console.log(err);
       }
     );
-    
   }
 
 }
